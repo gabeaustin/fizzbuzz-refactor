31bcf1907734c96a3f5c564edaf3648533f97ed1
Gabe Austin <gabriel.austin1@gmail.com>
2015-02-15 00:27:36 -0600

    add code to check if input from user is a number

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -17,3 +17,19 @@ var fizzBuzz = function(num){
     }
   }
 };
+
+// document.write will print the user's response 
+// to the screen.
+// I think this may complete some of the task:
+// var runFizzBuzz = function(){
+//   var input = prompt("Enter in a number.");
+  
+//   if (isNaN(input) || input % 1 !=0) {
+//     document.write("This is not a valid number.  Please supply a valid number.");
+//   } else {
+//     document.write("This is a number.");
+//   }
+  
+//   input = +input;
+//   fizzBuzz(input);
+// };
